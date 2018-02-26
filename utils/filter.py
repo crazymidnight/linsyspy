1f80100eefe2da445fac67dc5f8e68fe01179d27
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2018-02-26 12:48:36 +0700

    add utils

---
+++ utils/filter.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy
+
+
+
+if __name__ == '__main__':
+    print('try to import this package!')
---
+++ utils/stability.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def stability(A, B):
+    
+    pass
