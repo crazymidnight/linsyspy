0a74da88cb9994b5a3391ba3cbc25b1e3a7b4bb7
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2018-02-28 13:02:27 +0700

    remove news.py

--- news.py
+++
@@ -1,9 +0,0 @@
-import control
-from utils import get_data
-
-path = 'input.txt'
-
-A, B, C, D, E, F, X, U, N, Y, T = get_data.build_model(path)
-system = control.ss(A, B, C, D)
-
-print(control.stability_margins(system))
